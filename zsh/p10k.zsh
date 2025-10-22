b0a050f2e3ac98378a8286c4074d7cd303671e57
Sloan D. Mergler <smergler@amazon.com>
2025-10-22 14:45:52 -0400

    move battery to above line

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -108,12 +108,12 @@
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
     time                    # current time
+    battery               # internal battery
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
